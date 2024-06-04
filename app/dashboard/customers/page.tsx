30ebc5bf592bae1b9faad12638f7804e1ce5429f
ed <ed@email.com>
2024-06-04 17:39:41 -0400

    chapter 13

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,41 @@
 export default function Page() {
-  return <p>Customers Page</p>;
+  return     <>
+  <form>
+      <fieldset>
+          <legend>Biological Sex</legend>
+          <label>
+              <input type="radio" name="biologicalSex" value="male" required/> Male
+          </label><br/>
+          <label>
+              <input type="radio" name="biologicalSex" value="female"/> Female
+          </label>
+      </fieldset>
+      <br/><br/>
+      <fieldset>
+          <legend>Identify as</legend>
+          <label>
+              <input type="radio" name="identifyAs" value="male" required/> Male
+          </label><br/>
+          <label>
+              <input type="radio" name="identifyAs" value="female"/> Female
+          </label><br/>
+          <label>
+              <input type="radio" name="identifyAs" value="nonbinary"/> Non-Binary
+          </label>
+      </fieldset>
+      <br/><br/>
+      <fieldset>
+          <legend>Looking For</legend>
+          <label>
+              <input type="checkbox" name="lookingFor" value="male"/> Male
+          </label><br/>
+          <label>
+              <input type="checkbox" name="lookingFor" value="female"/> Female
+          </label><br/>
+          <label>
+              <input type="checkbox" name="lookingFor" value="nonbinary"/> Non-Binary
+          </label>
+      </fieldset> 
+      <br/><br/>
+  </form></>;
 }
