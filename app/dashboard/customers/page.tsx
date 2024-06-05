2c1afd65b1c767c075963658c6b312972ff4cfe7
ed <ed@email.com>
2024-06-05 14:36:34 -0400

    final chapter and adding some metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,41 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
 export default function Page() {
-  return     <>
-  <form>
-      <fieldset>
-          <legend>Biological Sex</legend>
-          <label>
-              <input type="radio" name="biologicalSex" value="male" required/> Male
-          </label><br/>
-          <label>
-              <input type="radio" name="biologicalSex" value="female"/> Female
-          </label>
-      </fieldset>
-      <br/><br/>
-      <fieldset>
-          <legend>Identify as</legend>
-          <label>
-              <input type="radio" name="identifyAs" value="male" required/> Male
-          </label><br/>
-          <label>
-              <input type="radio" name="identifyAs" value="female"/> Female
-          </label><br/>
-          <label>
-              <input type="radio" name="identifyAs" value="nonbinary"/> Non-Binary
-          </label>
-      </fieldset>
-      <br/><br/>
-      <fieldset>
-          <legend>Looking For</legend>
-          <label>
-              <input type="checkbox" name="lookingFor" value="male"/> Male
-          </label><br/>
-          <label>
-              <input type="checkbox" name="lookingFor" value="female"/> Female
-          </label><br/>
-          <label>
-              <input type="checkbox" name="lookingFor" value="nonbinary"/> Non-Binary
-          </label>
-      </fieldset> 
-      <br/><br/>
-  </form></>;
+  return <>Customers Page</>;
 }
